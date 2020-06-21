fd571f35c7e5d8fe2a37e2376f3fff219f7b26d4
Adam <adam.krzanowski1998@gmail.com>
2020-06-21 10:13:48 +0200

    Poprawki

--- src/main/java/covid/maps/controller/MapController.java
+++ src/main/java/covid/maps/controller/MapController.java
@@ -1,23 +1,29 @@
 package covid.maps.controller;
 
 import covid.maps.service.points.PointsService;
+import covid.maps.service.scheduler.ScheduledCovidDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.IOException;
+
 @Controller
 public class MapController {
 
     private final PointsService pointsService;
+    private final ScheduledCovidDataService scheduledCovidDataService;
 
     @Autowired
-    public MapController(PointsService pointsService) {
+    public MapController(PointsService pointsService, ScheduledCovidDataService scheduledCovidDataService) {
         this.pointsService = pointsService;
+        this.scheduledCovidDataService = scheduledCovidDataService;
     }
 
     @GetMapping
-    public String getMap(Model model) {
+    public String getMap(Model model) throws IOException {
+        scheduledCovidDataService.loadActualDataToDatabase();
         model.addAttribute("data", pointsService.getListPoints());
         return "map";
     }
